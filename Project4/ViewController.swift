5f18cabdb22515aa6b4269cfce87ca9954b15dd4
Ning Xinran <xinran.ning@viki.com>
2023-05-22 15:49:28 +0800

    Initial Commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Ning, Xinran on 22/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
